ec92e939f67fb6e4fa881e091aaa1a35322e7c6c
kashiwabarayuta <alfamod47@yahoo.co.jp>
2026-02-20 04:34:49 +0900

    リファクタリング

--- Assets/Scripts/TitleGameManger.cs
+++ Assets/Scripts/TitleGameManger.cs
@@ -3,29 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class TitleGameManger : MonoBehaviour
 {
-    void Start()
-    {
-        Screen.SetResolution(1920, 1080, true, 60);
+	void Start()
+	{
+		Screen.SetResolution(1920, 1080, true, 60);
 
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
 
-        if (Input.anyKey)
-        {
-            SceneManager.LoadScene("PlayScene");
-        }
-    }
-
-    public void ToPlayScene()
-    {
-        SceneManager.LoadScene("PlayScene");
-    }
+		if (Input.anyKey)
+		{
+			SceneManager.LoadScene("PlayScene");
+		}
+	}
 }
